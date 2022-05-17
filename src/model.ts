8b6f396c6a74fd486ae5d43579dd148aaa8700b1
tekihei2317 <tekihei2317@gmail.com>
2022-05-17 20:37:02 +0900

    feat: define model types

---
+++ src/model.ts
@@ -0,0 +1,26 @@
+export type User = {
+  id: string
+  name: string
+}
+
+export type ProblemDifficulty = 'Easy' | 'Medium' | 'Hard' | 'Extreme'
+
+export type Problem = {
+  id: string
+  title: string
+  content: string
+  difficulty: ProblemDifficulty
+  originalUrl: string
+}
+
+export type SubmissionStatus = 'Judging' | 'Accepted' | 'Wrong Answer'
+
+export type Submission = {
+  id: string
+  problemId: string
+  user: User
+  status: SubmissionStatus
+  codeSize: number
+  commentary?: string
+  createdAt: Date
+}
