0752b5d78ea6d79f47af4fedba16c1103679b67e
tekihei2317 <tekihei2317@gmail.com>
2022-05-17 20:49:28 +0900

    feat: install react-router-dom, and define routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ProblemsPage } from './pages/problems'
+import { IndexPage } from './pages'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<IndexPage />} />
+          <Route path="/problems" element={<ProblemsPage />} />
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
 )
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Text } from '@chakra-ui/react'
+
+export const IndexPage = () => {
+  return (
+    <Container maxW={'container.xl'}>
+      <Text fontSize="2xl" fontWeight={'bold'}>
+        Type Challenges Judge
+      </Text>
+    </Container>
+  )
+}
