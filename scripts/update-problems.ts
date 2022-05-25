ae3a6d1aeb3297ebe86869bc4e0ddc12781db801
tekihei2317 <tekihei2317@gmail.com>
2022-05-25 20:50:02 +0900

    fix: update problem title, and add syntax highlighting

--- scripts/update-problems.ts
+++ scripts/update-problems.ts
@@ -3,21 +3,25 @@ import { db } from './utils/firebase'
 import { loadProblems } from './utils/loader'
 
 export async function updateProblems() {
-  // TODO:
   const quizez = loadProblems()
 
-  quizez.map((quiz) => {
+  quizez.forEach((quiz) => {
     if (quiz.difficulty === 'pending') return
+
+    const title = quiz.info?.en?.title
+    if (title === undefined) return
 
     const problem: Problem = {
       id: quiz.path,
-      title: quiz.path,
+      title,
       content: quiz.readme.en,
       difficulty: quiz.difficulty,
     }
 
     db.collection(collectionName.problems).doc(problem.id).set(problem)
   })
+
+  console.log('問題を更新しました')
 }
 
 updateProblems()
--- src/pages/problems/[problemId]/index.tsx
+++ src/pages/problems/[problemId]/index.tsx
@@ -1,13 +1,54 @@
-import { Container, Text } from '@chakra-ui/react'
+import ReactMarkdown from 'react-markdown'
 import { useOutletContext } from 'react-router-dom'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { dracula as syntaxStyle } from 'react-syntax-highlighter/dist/esm/styles/prism'
+
+import {
+  CodeComponent,
+  CodeProps,
+  ReactMarkdownNames,
+} from 'react-markdown/lib/ast-to-react'
 import { ProblemLayoutContext } from '../../../components/ProblemLayout'
+
+const CodeBlock: CodeComponent | ReactMarkdownNames = ({
+  inline,
+  className,
+  children,
+  ...props
+}: CodeProps) => {
+  const match = /language-(\w+)/.exec(className || '')
+
+  if (inline || !match)
+    return (
+      <code className={className} {...props}>
+        {children}
+      </code>
+    )
+
+  return (
+    <SyntaxHighlighter
+      style={syntaxStyle}
+      language={match[1]}
+      PreTag="div"
+      {...props}
+    >
+      {children}
+    </SyntaxHighlighter>
+  )
+}
 
 export const ProblemPage = () => {
   const { problem } = useOutletContext<ProblemLayoutContext>()
 
   return (
-    <Container maxW={'container.xl'}>
-      <Text>{problem?.content}</Text>
-    </Container>
+    <>
+      {problem === undefined ? (
+        <></>
+      ) : (
+        <ReactMarkdown components={{ code: CodeBlock }}>
+          {problem.content}
+        </ReactMarkdown>
+      )}
+    </>
   )
 }
