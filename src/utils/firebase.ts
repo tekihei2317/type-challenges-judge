edd673b94cfbe79b3d77cb831f2f071898077977
tekihei2317 <tekihei2317@gmail.com>
2022-06-02 19:21:50 +0900

    fix: added security rules for problems

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
+import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -14,3 +14,7 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db = getFirestore(app)
+
+if (import.meta.env.MODE === 'development') {
+  connectFirestoreEmulator(db, 'localhost', 8080)
+}
