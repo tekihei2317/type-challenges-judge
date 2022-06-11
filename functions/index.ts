68d751801a29c0a5503ab4a7a8a8b4565e7763fd
tekihei2317 <tekihei2317@gmail.com>
2022-06-11 14:45:05 +0900

    fix: 不要なFirebase Functionを削除 & 関数とセキュリティルールのデプロイ処理を追加

--- functions/index.ts
+++ functions/index.ts
@@ -2,6 +2,4 @@ import admin from 'firebase-admin'
 
 admin.initializeApp()
 
-module.exports.addMessage = require('./functions/add-message')
-module.exports.makeUppercase = require('./functions/make-uppercase')
 module.exports.addSubmissionProblem = require('./functions/add-submission-problem')
